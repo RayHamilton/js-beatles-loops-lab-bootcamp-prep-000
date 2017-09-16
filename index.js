e3be94e5c55cbaa80d0783c0d4322963c54fcbcb
Ray Hamilton <hamilton.ray@gmail.com>
2017-09-16 12:36:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ function johnLennonFacts(arr){
   }
   return arr
 }
+
+function iLoveTheBeatle(num){
+  var arr = []
+  do {
+    arr.push("I love the Beatles!")
+    num++
+  } while num < 15
+  return arr
+}
